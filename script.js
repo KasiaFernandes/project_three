e28e62f783b6ec7f6feb3f3842c6648a9e9b8ec2
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-12-07 21:41:55 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$("#white-camara").on('click', function() {
+    $(".navbar-nav").slideToggle();
+});
+
+
+/*
+$("#carousel-1").on("slid.bs.carousel", function() {
+    var totalSlides = $(".item").length;
+    var currentSlide = $("div.active").index() + 1;
+    var text = 
+    $("#carousel-number").append(<h1>Hello</h1>);
+})
+*/
+});
